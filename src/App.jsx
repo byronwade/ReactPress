dbc0305413d130abea879d1a20210e6a9c584e11
Byron Wade <bcw1995@gmail.com>
2023-02-27 19:41:38 -0800

    added vite to the project and got it rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,41 @@
 import React, { Suspense } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import AllRoutes from "./pages/routes";
-import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
-
-const client = new ApolloClient({
-	link: createHttpLink({ uri: "https://jsonplaceholder.typicode.com" }),
-	cache: new InMemoryCache(),
-});
 
 const App = () => {
 	return (
-		<ApolloProvider client={client}>
 			<Router>
 				<Suspense fallback={<div>Loading...</div>}>
 					<AllRoutes />
 				</Suspense>
 			</Router>
-		</ApolloProvider>
 	);
 };
 
 export default App;
+
+
+
+// import React, { Suspense } from "react";
+// import { BrowserRouter as Router } from "react-router-dom";
+// import AllRoutes from "./pages/routes";
+// import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+
+// const client = new ApolloClient({
+// 	link: createHttpLink({ uri: "https://jsonplaceholder.typicode.com" }),
+// 	cache: new InMemoryCache(),
+// });
+
+// const App = () => {
+// 	return (
+// 		<ApolloProvider client={client}>
+// 			<Router>
+// 				<Suspense fallback={<div>Loading...</div>}>
+// 					<AllRoutes />
+// 				</Suspense>
+// 			</Router>
+// 		</ApolloProvider>
+// 	);
+// };
+
+// export default App;
